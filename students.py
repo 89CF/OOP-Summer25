8362d31b382a0da243b56739474398ce45f4cf95
BSoD <76407609+89CF@users.noreply.github.com>
2025-03-13 11:16:16 +0100

    Update students.py

--- students.py
+++ students.py
@@ -1,27 +1,27 @@
 students = [
- {
- 'first_name' = 'Mehmet',
-'last_name' = 'Konmaz',
-'index_number' = 'TR30011',
-'nationality' = 'Turkey',
-'starting_date' = '2024-11-01',
-'courses' = ['Computer security', 'Match','Ethics']
+{
+ 'first_name' : 'Mehmet',
+'last_name' : 'Konmaz',
+'index_number' : 'TR30011',
+'nationality' : 'Turkey',
+'starting_date' : '2024-11-01',
+'courses' : ['Computer security', 'Match','Ethics']
 },
 {
- 'first_name' = 'Berk',
-'last_name' = 'Oldu',
-'index_number' = 'TR2202',
-'nationality' = 'Turkey',
-'starting_date' = '2024-09-01',
-'courses' = ['Computer Science', 'Match','Ethics']
+ 'first_name' : 'Berk',
+'last_name' : 'Oldu',
+'index_number' : 'TR2202',
+'nationality' : 'Turkey',
+'starting_date' : '2024-09-01',
+'courses' : ['Computer Science', 'Match','Ethics']
 },
 {
- 'first_name' = 'Ata',
-'last_name' = 'Ucmaz',
-'index_number' = 'TR32551',
-'nationality' = 'Turkey',
-'starting_date' = '2024-01-01',
-'courses' = ['English', 'Match','Ethics']
+ 'first_name' : 'Ata',
+'last_name' : 'Ucmaz',
+'index_number' : 'TR32551',
+'nationality' : 'Turkey',
+'starting_date' : '2024-01-01',
+'courses' : ['English', 'Match','Ethics']
 }
 ]
 
