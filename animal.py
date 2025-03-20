5d443a806ed61221ca29225102f01a56d3140a6d
BSoD <76407609+89CF@users.noreply.github.com>
2025-03-20 10:37:06 +0100

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,32 @@
+animals = [
+    {
+        'name': 'Cat',
+        'group': 'mammals',
+        'number_of_legs': 4,
+        'skills': ['climbing', 'night vision', 'agile hunting']
+    },
+    {
+        'name': 'Eagle',
+        'group': 'birds',
+        'number_of_legs': 2,
+        'skills': ['flying', 'keen eyesight', 'soaring']
+    },
+    {
+        'name': 'Frog',
+        'group': 'amphibians',
+        'number_of_legs': 4,
+        'skills': ['jumping', 'swimming', 'camouflage']
+    },
+    {
+        'name': 'Octopus',
+        'group': 'cephalopods',
+        'number_of_legs': 0,
+        'skills': ['problem solving', 'ink ejection', 'color changing']
+    },
+    {
+        'name': 'Crocodile',
+        'group': 'reptiles',
+        'number_of_legs': 4,
+        'skills': ['ambush hunting', 'swimming', 'powerful bite']
+    }
+]
